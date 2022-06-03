641676b227856432ec60bbab1ff18578c0f21fcc
woojin9606 <qordnwls11@gmail.com>
2022-06-03 19:59:44 +0900

    게시물 리스팅 기능 구현

--- article/views.py
+++ article/views.py
@@ -17,13 +17,14 @@ def article(request):
     if request.method == 'GET':
         user = request.user.is_authenticated
         if user:
-            all_tweet = ArticleModel.objects.all().order_by('-created_at')
-            return render(request, 'article/upload.html', {'tweet': all_tweet})
+            all_article = ArticleModel.objects.all()
+            return render(request, 'article/home.html', {'article': all_article})
         else:
             return redirect('/sign-in')
     elif request.method == 'POST':
         user = request.user
         title = request.POST.get('title', '')
+        poster = request.POST.get('poster', '')
         synopsis = request.POST.get('synopsis', '')
         genre = request.POST.get('genre', '')
         cast = request.POST.get('cast', '')
@@ -31,7 +32,7 @@ def article(request):
         episode = request.POST.get('episode', '')
         aged = request.POST.get('aged', '')
 
-        my_article = ArticleModel.objects.create(author=user, title=title, synopsis=synopsis, genre=genre,
+        my_article = ArticleModel.objects.create(author=user,poster=poster, title=title, synopsis=synopsis, genre=genre,
                                                  cast=cast, aired_date=aired_date, episode=episode, aged=aged, rating = 0)
         my_article.save()
-        return redirect('/tweet')
+        return redirect('/article')
